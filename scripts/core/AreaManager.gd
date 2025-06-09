9bf51530091c25f7e06bb973e9bec81da865e507
Taroon Vijay <ZLAC209@live.rhul.ac.uk>
2025-06-09 18:16:37 +0100

    Updated #1: Added backend logic to handle Area and races done progression. Need to link with UI and add optionals.

--- scripts/core/AreaManager.gd
+++ scripts/core/AreaManager.gd
@@ -1,4 +1,23 @@
 extends Node
 
-var currentArea := 0
-var racesDoneforArea := 0
+var currentArea = 0
+var racesDoneforArea = 0
+var areas = ["", "", ""]
+var areaRequirements = [2, 4, 6, 8]
+var areaMultiplier = 0
+
+func raceComplete():
+	racesDoneforArea += 1
+	if racesDoneforArea >= areaRequirements[currentArea]:
+		unlockNextArea()
+		
+
+func unlockNextArea():
+	if currentArea < areaRequirements.size() - 1:
+		currentArea += 1
+		areaMultiplier += 0.5
+		racesDoneforArea = 0
+		print("Congratulations! You have unlocked a new Area : %s" % (areas[currentArea] + 1))
+	else:
+		print("Congratulations! You have unlocked the Final Area : %s" % (areas[-1]))
+	
